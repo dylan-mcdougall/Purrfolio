963c9b523950aeb31cc27899fe9eb1546041ec48
Your Name <dylan.7mcdougall@gmail.com>
2023-09-19 10:44:09 -0700

    minor styling changes to navigation -dylan

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -13,6 +13,7 @@ function Navigation() {
 	const logout = (e) => {
 		e.preventDefault();
 		dispatch(sessionActions.logout());
+		history.push('/')
 	};
 
 	if (!sessionUser) return null;
@@ -36,6 +37,7 @@ function Navigation() {
 					</div>
 					<div className='navigation-layer'>
 						<li className='navigation-item add-funds'>
+							<button>
 							<div className='nav-content-wrapper'>
 								<div className='nav-svg'>
 									<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path fill='#FFFFFF' d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" /></svg>
@@ -44,6 +46,7 @@ function Navigation() {
 									Add Funds
 								</div>
 							</div>
+							</button>
 						</li>
 					</div>
 					<div className='navigation-layer'>
