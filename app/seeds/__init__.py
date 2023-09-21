802313bf17274f832417ae6cb801e6318a1be74e
Dylan <dylan.7mcdougall@gmail.com>
2023-09-20 18:22:06 -0700

    stock seeder api calls set, still needs error handling and to be trimmed down for speed

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .stocks import seed_stocks, undo_stocks
 
 from app.models.db import db, environment, SCHEMA
 
@@ -18,6 +19,7 @@ def seed():
         # Make sure to add all your other model's undo functions below
         undo_users()
     seed_users()
+    seed_stocks()
     # Add other seed functions here
 
 
@@ -25,4 +27,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_stocks()
     # Add other undo functions here
--- app/seeds/stocks.py
+++ app/seeds/stocks.py
@@ -3,6 +3,7 @@ from sqlalchemy.sql import text
 import os
 import requests
 
+KEY = os.environ.get("KEY")
 SECRET = os.environ.get("SECRET")
 
 seed_data = [
@@ -18,13 +19,65 @@ seed_data = [
     'HBAN', 'BMY', 'MU', 'MRO', 'SIRI', 'VTRS', 'KEY', 'BHC', 'BCS'
 ]
 
-robust_seed_data = []
+stock_names = [
+    'NIO Inc.', 'Tesla Inc.', 'Vale S.A.', 'Amazon.com Inc.', 'Intel Corp.', 'Apple Inc.', 'Avinger Inc.', 'Advanced Micro Devices Inc.', 'Ford Motor Co.',
+    'Palantir Technologies Inc.', 'NVIDIA Corp.', 'Bank of America Corp.', 'Baytex Energy Corp.', 'Rivian Automotive Inc.', 'Pinterest Inc.', 'AT&T Inc.', 'Pfizer Inc.', 'Petróleo Brasileiro S.A. - Petrobras', 'Lucid Group Inc.',
+    'Tilray Inc.', 'American Airlines Group Inc.', 'Marathon Digital Holdings Inc.', 'Carnival Corp.', 'AMC Entertainment Holdings Inc.', 'Verizon Communications Inc.', 'Grab Holdings Ltd', 'The Walt Disney Co.', 'Kenvue Inc', 'Alphabet Inc. (Class A)',
+    'Snap Inc.', 'Southwestern Energy Co.', 'Uber Technologies Inc.', 'Riot Blockchain Inc.', 'SoFi Technologies Inc.', 'Rackspace Technology Inc.', 'Square Inc.', 'Rocket Lab USA Inc', 'Microsoft Corp.', 'Aurora Innovation Inc',
+    'Transocean Ltd.', 'Eos Energy Enterprises Inc', 'Meta Platforms Inc.', 'Comcast Corp.', 'Nokia Corp.', 'Affirm Holdings Inc.', 'JetBlue Airways Corp.', 'Alphabet Inc. (Class C)', 'PG&E Corp.', 'Wells Fargo & Co.',
+    'CSX Corp.', 'Barrick Gold Corp.', 'XPeng Inc.', 'Roblox Corp.', 'Itaú Unibanco Holding S.A.', 'Kinder Morgan Inc.', 'Uranium Energy Corp.', 'Cleveland-Cliffs Inc.', 'Shopify Inc.', 'Carvana Co.',
+    'Revance Therapeutics Inc.', 'Farfetch Ltd.', 'Freeport-McMoRan Inc.', 'Macy’s Inc.', 'Virgin Galactic Holdings Inc.', 'Peloton Interactive Inc.', 'Equinox Gold Corp', 'Exxon Mobil Corp.', 'Ambev S.A.', 'Nu Holdings Ltd',
+    'TechnipFMC PLC', 'United States Steel Corp.', 'Plug Power Inc.', 'The Coca-Cola Co.', 'Norwegian Cruise Line Holdings Ltd.', 'CRH PLC ADR', 'Opendoor Technologies Inc.', 'PayPal Holdings Inc.', 'Raytheon Technologies Corp.', 'Unity Software Inc', 'CleanSpark Inc', 
+    'Coupang Inc.', 'Citigroup Inc.', 'Medical Properties Trust Inc.', 'U.S. Bancorp', 'Cisco Systems Inc.', 'ChargePoint Holdings Inc.', 'Warner Bros Discovery Inc', 'Truist Financial Corp.', 'Walgreens Boots Alliance Inc.', 'Novavax Inc.', 'Newmont Corp.',
+    'Huntington Bancshares Inc.', 'Bristol-Myers Squibb Co.', 'Micron Technology Inc.', 'Marathon Oil Corp.', 'Sirius XM Holdings Inc.', 'Viatris Inc.', 'KeyCorp', 'Bausch Health Companies Inc.', 'Barclays PLC'
+]
 
+robust_seed_data = []
+final_seed_data = []
 
 for i in seed_data:
-    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={i}&apikey={SECRET}'
-    r = requests.get(url)
+    url = f"https://data.alpaca.markets/v2/stocks/snapshots?symbols={i}&feed=iex"
+    headers = {
+    "accept": "application/json",
+    "APCA-API-KEY-ID": KEY,
+    "APCA-API-SECRET-KEY": SECRET
+    }
+    r = requests.get(url, headers=headers)
     data = r.json()
     robust_seed_data.append(data)
 
-print robust_seed_data
+for i, ticker in enumerate(seed_data):
+    x = {}
+    data = robust_seed_data[i]
+
+    x['name'] = stock_names[i]
+    x['ticker'] = ticker
+    x['price'] = data.get(ticker, {}).get('latestTrade', {}).get('p', None)
+    x['open'] = data.get(ticker, {}).get('dailyBar', {}).get('o', None)
+    x['close'] = data.get(ticker, {}).get('prevDailyBar', {}).get('c', None)
+    x['high'] = data.get(ticker, {}).get('dailyBar', {}).get('h', None)
+    x['low'] = data.get(ticker, {}).get('dailyBar', {}).get('l', None)
+    x['volume'] = data.get(ticker, {}).get('dailyBar', {}).get('v', None)
+
+    final_seed_data.append(x)
+
+def seed_stocks():
+    stocks = [
+        Stock(
+            name=i['name'], ticker=i['ticker'], price=i['price'],
+            open=i['open'], close=i['close'], high=i['high'], 
+            low=i['low'], volume=i['volume']
+        )
+    for i in final_seed_data
+    ]
+    for i in stocks:
+        db.session.add(i)
+    db.session.commit()
+
+def undo_stocks():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.stocks RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM stocks"))
+        
+    db.session.commit()
