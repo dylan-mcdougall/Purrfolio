a154cc01de5de6f0d6c1e0829cdab42c4d7c437e
Your Name <dylan.7mcdougall@gmail.com>
2023-09-21 16:34:43 -0700

    temporary endpoint for development

--- app/api/portfolio_routes.py
+++ app/api/portfolio_routes.py
@@ -31,3 +31,22 @@ def portfolio_stocks(id):
     portfolio = Portfolio.query.get(id)
     stocks = [stock.to_dict() for stock in portfolio.stocks]
     return jsonify({"stocks": stocks})
+
+@portfolio_routes.route('/<int:id>/stock-valuation')
+@login_required
+def portfolio_stock_valuation(id):
+    """
+    Query to return the total value of assets owned by portfolio
+    """
+    portfolio = Portfolio.query.get(id)
+    portfolio_stocks = [stock.to_dict() for stock in portfolio.stocks]
+    stock_ids = [s['stock_id'] for s in portfolio_stocks]
+    stocks = Stock.query.filter(Stock.id.in_(stock_ids)).all()
+    stock_dict = {stock.id: stock for stock in stocks}
+
+    value = 0
+    for i in portfolio_stocks:
+        stock = stock_dict.get(i['stock_id'])
+        if stock:
+            value += stock.price * i['quantity']
+    return jsonify({"value": value})
