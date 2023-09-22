2234cb94e788d7a0a6399748dc15dd3a5f3ed5be
Your Name <dylan.7mcdougall@gmail.com>
2023-09-21 18:12:49 -0700

    updated portfolio details api route to better reflect development needs

--- app/api/portfolio_routes.py
+++ app/api/portfolio_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User, Portfolio, Stock
+from app.models import User, Portfolio, Stock, Transaction
 
 portfolio_routes = Blueprint('portfolios', __name__)
 
@@ -15,12 +15,37 @@ def portfolios():
 
 @portfolio_routes.route('/<int:id>')
 @login_required
-def portfolio(id):
+def portfolio_details(id):
     """
     Query a portfolio by id and returns that portfolio
     """
     portfolio = Portfolio.query.get(id)
-    return portfolio.to_dict()
+    portfolio_dict = portfolio.to_dict()
+
+    portfolio_stocks = [stock.to_dict() for stock in portfolio.stocks]
+
+    portfolio_dict['stocks'] = portfolio_stocks
+    
+    stock_ids = [s['stock_id'] for s in portfolio_stocks]
+    stocks = Stock.query.filter(Stock.id.in_(stock_ids)).all()
+    stock_dict = {stock.id: stock for stock in stocks}
+
+    value = 0
+    for i in portfolio_stocks:
+        stock = stock_dict.get(i['stock_id'])
+        if stock:
+            value += stock.price * i['quantity']
+
+    portfolio_dict['portfolio_valuation'] = value
+    portfolio_dict['total_valuation'] = value + portfolio_dict['current_funds']
+
+    transactions = [transaction.to_dict() for transaction in portfolio.transactions]
+    portfolio_dict['transactions'] = transactions
+
+    user = portfolio.user.to_dict()
+    portfolio_dict['user'] = user
+
+    return portfolio_dict
 
 @portfolio_routes.route('/<int:id>/stocks')
 @login_required
@@ -31,22 +56,3 @@ def portfolio_stocks(id):
     portfolio = Portfolio.query.get(id)
     stocks = [stock.to_dict() for stock in portfolio.stocks]
     return jsonify({"stocks": stocks})
-
-@portfolio_routes.route('/<int:id>/stock-valuation')
-@login_required
-def portfolio_stock_valuation(id):
-    """
-    Query to return the total value of assets owned by portfolio
-    """
-    portfolio = Portfolio.query.get(id)
-    portfolio_stocks = [stock.to_dict() for stock in portfolio.stocks]
-    stock_ids = [s['stock_id'] for s in portfolio_stocks]
-    stocks = Stock.query.filter(Stock.id.in_(stock_ids)).all()
-    stock_dict = {stock.id: stock for stock in stocks}
-
-    value = 0
-    for i in portfolio_stocks:
-        stock = stock_dict.get(i['stock_id'])
-        if stock:
-            value += stock.price * i['quantity']
-    return jsonify({"value": value})
