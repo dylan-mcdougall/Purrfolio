c0d7ac431f88a40d941f551ad1ead1c7585d69ec
Dylan <dylan.7mcdougall@gmail.com>
2023-11-06 15:47:43 -0800

    updated navigation UX and responsiveness

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
 
     return (
         <div className='footer-wrapper'>
-          <p>Created by <a href='https://github.com/eanorman'>Emily Norman</a>, <a href=''>Charles Carroll</a>, and <a href='https://github.com/dylan-mcdougall'>Dylan McDougall</a>.</p>
+          <p>Created by <a href='https://github.com/eanorman'>Emily Norman</a>, <a href='https://github.com/C-Carroll'>Charles Carroll</a>, and <a href='https://github.com/dylan-mcdougall'>Dylan McDougall</a>.</p>
         </div>
     )
 }
