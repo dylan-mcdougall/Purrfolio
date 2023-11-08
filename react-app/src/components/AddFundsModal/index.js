61590b6e9d3dba6a1792433d307121150f9ffeda
Dylan <dylan.7mcdougall@gmail.com>
2023-11-07 16:07:51 -0800

    added error handling for add-funds modal

--- react-app/src/components/AddFundsModal/index.js
+++ react-app/src/components/AddFundsModal/index.js
@@ -11,6 +11,7 @@ function AddFundsModal() {
     const sessionPortfolio = useSelector((state) => state.portfolio.portfolio);
     const sessionUser = useSelector((state) => state.session.user);
     const [currentFunds, setCurrentFunds] = useState(0);
+    const [errors, setErrors] = useState(null);
     const dispatch = useDispatch();
     const { closeModal } = useModal();
 
@@ -19,7 +20,13 @@ function AddFundsModal() {
     }, [sessionPortfolio])
 
     async function handleClick(e) {
+        setErrors(null)
         e.preventDefault()
+        if (amount <= 0) {
+            return setErrors({
+                "amount": "Must enter an amount between 0 and 10,000,000."
+            })
+        }
         const portfolioId = sessionPortfolio.portfolio.id
         const formData = {
             funds: amount
@@ -50,6 +57,9 @@ function AddFundsModal() {
                     <div className="add-funds-input-wrapper">
                         <label className="add-funds-label" htmlFor="amount">Desired Amount: </label>
                         <input className="add-funds-input" type="number" name="amount" min="0" defaultValue={amount} onChange={(e) => setAmount(e.target.value)}></input>
+                    </div>
+                    <div className="errors">
+                        {errors && <p className="error">{errors.amount}</p>}
                     </div>
                     <p className="add-funds-p">* This is paper money, no real trades will be executed.</p>
                     <div className="add-funds-submit-wrapper">
