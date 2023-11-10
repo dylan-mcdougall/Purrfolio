1cbb097991fcd4cf5bcf06b0c736208baf268be8
Dylan <dylan.7mcdougall@gmail.com>
2023-11-10 12:03:03 -0800

    updated logout button functionality to flash red on mouseover

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import './Navigation.css';
@@ -10,10 +10,28 @@ function Navigation() {
 	const sessionUser = useSelector(state => state.session.user);
 	const sessionPortfolio = useSelector((state) => state.portfolio.portfolio);
 	const [currentFunds, setCurrentFunds] = useState(0);
+	const [hover, setHover] = useState(false);
+	const logoutRef = useRef();
 
 	const dispatch = useDispatch();
 	const history = useHistory();
 
+	
+	useEffect(() => {
+		const handleHover = (e) => {
+			if (logoutRef.current && logoutRef.current.contains(e.target)) {
+				setHover(true);
+			} else {
+				setHover(false);
+			}
+		};
+	
+		document.addEventListener("mouseover", handleHover);
+	
+		return () => document.removeEventListener("mouseover", handleHover);
+	}, [logoutRef]);
+	
+	
 	const logout = (e) => {
 		e.preventDefault();
 		dispatch(sessionActions.logout());
@@ -26,6 +44,12 @@ function Navigation() {
 		}
 	}, [setCurrentFunds])
 
+	let logoutBase = 'logout'
+	let logoutHover = 'logout'
+
+	logoutBase = hover ? logoutBase + ' hidden' : logoutBase
+	logoutHover = hover ? logoutHover : logoutHover + ' hidden'
+	
 	if (!sessionUser) return null;
 	else return (
 		<div className='nav-wrapper'>
@@ -105,10 +129,11 @@ function Navigation() {
 				</div>
 				<div className='nav-bottom'>
 					<div className='navigation-layer'>
-						<li className='navigation-item logout'>
+						<li className='navigation-item logout' ref={logoutRef}>
 							<div className='nav-content-wrapper' onClick={logout}>
 								<div className='nav-svg'>
-									<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path fill='#FFFFFF' d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" /></svg>
+									<svg className={logoutBase} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path fill='#FFFFFF' d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" /></svg>
+									<svg className={logoutHover} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path fill='#ff0000' d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"/></svg>
 								</div>
 								<div className='nav-context'>
 									Log Out
